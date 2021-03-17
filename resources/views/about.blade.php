bcfa241ea4fdc51e7732bc420914e58d3c9d4f11
thegigi13 <gigithebest@live.fr>
2021-03-17 18:15:00 +0100

    Add about page and update links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,14 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello City</title>
+        <title>about-us | Hello City</title>
 
 
     </head>
     <body>
-        <h1>Hello from Quebec!</h1>
+ 
 
-        <p>It's currently {{ date('h:i A') }}.</p>
+        <p>Built with &hearts; by Me.</p>
+        <p><a href="/">Revenir à la page d'accueil</a></p>
 
 
         <footer>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/',function (){
+	return view('home');
+});
+
+
+Route::get('/about-us',function (){
+	return view('about');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +35,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
+
 Route::get('/', function () {
     return view('home');
 });
+*/
