65f7a46d723e937022ba9ef3e9f0489f0c6bedef
thegigi13 <gigithebest@live.fr>
2021-03-19 12:08:24 +0100

    Remove ducplication by using app layout

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,23 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello City</title>
+@extends('base')
 
+@section('content')
 
-    </head>
-    <body>
         <h1>Hello from Quebec!</h1>
 
         <p>It's currently {{ date('h:i A') }}.</p>
 
-
-        <footer>
-            <p>&copy; Copyright {{ date('Y')}} &middot; <a href="/about-us">About US</a>
-                
-            </p>
-        </footer>
-    </body>
-</html>
+@endsection
