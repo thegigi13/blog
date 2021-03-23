83a5a01a26ead19763802d0d0b3ded7816201861
thegigi13 <gigithebest@live.fr>
2021-03-23 18:01:21 +0100

    Make use of Route::view instead of Route::get

--- app/helpers.php
+++ app/helpers.php
@@ -1,18 +1,23 @@
 <?php
 
-if(!function_exists('page_title')) {
-	function page_title(?string $title = null) : string {
-		if ($title = null) {
-			return config(app.name);
-		} else {
-			return $title . ' | ' . config('app.name');
-		}
+if (!function_exists('page_title')) {
+	function page_title(?string $title = null): string {
+		return $title ? $title . ' | ' . config('app.name') : config('app.name') ;
 	}
 }
 
 
 // oder code for return $title
 
-//  return $title ? $title . ' | ' . config('app.name') : config('app.name') ;
 
+/*
+
+
+if ($title == null) {
+			return config('app.name');
+		} else {
+			return $title . ' | ' . config('app.name');
+		}
  
+
+*/
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+/*
+
 Route::get('/',function (){
 	return view('pages/home');
 })->name('home');
@@ -12,13 +15,28 @@ Route::get('/about-us',function (){
 })->name('about');
 
 
+
 Route::get('/help', function(){
 	return view('pages/help');
 });
 
+*/
 
 
 
+/*  oder for code function for php langage
+*/
+
+// Route::get('/', fn() => view('pages/home')) -> name('home');
+// Route::get('/about-us', fn() => view('pages/about')) -> name('about');
+
+
+/* oder for code Route with Laravel
+*/
+
+Route::view('/', 'pages/home') -> name('home');
+Route::view('/about-us', 'pages.about') -> name('about');
+Route::view('/help', 'pages/help');
 
 
 
